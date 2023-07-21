69d9974976591c2dc19d7fdfc3578b0b581bf4c7
R-Kershaw <rkershaw90@gmail.com>
2023-07-20 23:37:27 -0400

    changed http to https for netlify security

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -8,7 +8,7 @@ export default function SelectedContact({ selectedContactId, setSelectedContactI
     useEffect(() => {
         async function fetchContact() {
             try {
-                const response = await fetch(`http://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
+                const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
                 const result = await response.json();
                 setContact(result);
             } catch (error) {
